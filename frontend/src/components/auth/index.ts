6124b06e8dd4db099654d521933d3bc36df5ead8
Nicolas Arbelaez <nicolas.arbelaez01@usc.edu.co>
2024-11-11 13:21:46 -0500

    Barrel in components

---
+++ frontend/src/components/auth/index.ts
@@ -0,0 +1,6 @@
+import AuthButton from "./AuthButton";
+import AuthForm from "./AuthForm";
+import LabeledInput from "./LabeledInput";
+import PasswordInput from "./PasswordInput";
+
+export { AuthButton, AuthForm, LabeledInput, PasswordInput };
